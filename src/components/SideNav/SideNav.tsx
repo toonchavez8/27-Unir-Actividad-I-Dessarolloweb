6e1f79839cfaac1d03773d29cd9e58a3ab412379
toonchavez8 <desarrollador5@sistemasicong.com>
2025-07-03 11:13:00 -0600

    refactor(app): update packages and change link to use rr link

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,6 +1,7 @@
 import Logo from "@/components/atomic/Logo";
 import { siteRoutes } from "@/Routes/siteRoutes";
 import "./_sideNav.css";
+import { Link } from "react-router";
 interface SideNavProps {
   isOpen?: boolean;
   onClose?: () => void;
@@ -12,11 +13,13 @@ const SideNav: React.FC<SideNavProps> = ({ isOpen = false, onClose }) => {
 
   return (
     <nav className={`sidenav ${isOpen ? "sidenav--open" : ""} `}>
-      <div className="sidenav__header debug">
-        <Logo />
+      <div className="sidenav__header ">
+        <Link to={"/"}>
+          <Logo />
+        </Link>
         {onClose && (
           <button
-            className="sidenav__close-btn debug"
+            className="sidenav__close-btn "
             onClick={onClose}
             aria-label="Close navigation"
           >
@@ -30,13 +33,13 @@ const SideNav: React.FC<SideNavProps> = ({ isOpen = false, onClose }) => {
           const IconComponent = route.Icon;
           return (
             <li key={route.id} className="sidenav__item ">
-              <a href={route.path} className="sidenav__link">
+              <Link to={route.path} className="sidenav__link">
                 {IconComponent && <IconComponent />}
                 <span className="sidenav__text">
                   {route.path.slice(1).charAt(0).toUpperCase() +
                     route.path.slice(2)}
                 </span>
-              </a>
+              </Link>
             </li>
           );
         })}
